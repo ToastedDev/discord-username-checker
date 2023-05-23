00526e4eff76daa876bcef40656f0c3432cde387
Not TCA <realtcatech@gmail.com>
2023-05-23 07:21:11 +0000

    add form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+import { Input } from "~/components/input";
+
 export default function Home() {
   return (
     <>
       <h1 className="text-5xl font-bold">Discord Username Checker</h1>
       <p>Check if your @username is taken or not.</p>
+      <form className="flex flex-grow flex-col justify-center">
+        <Input placeholder="Enter your desired username..." />
+      </form>
     </>
   );
 }
